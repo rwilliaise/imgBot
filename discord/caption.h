da0888d3797c04593c05021a3b262c099aaf385b
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2023-04-25 17:30:25 -0700

    Add small stuff for bot commands

---
+++ discord/caption.h
@@ -0,0 +1,11 @@
+
+#ifndef DISCORD_CAPTION_H_
+#define DISCORD_CAPTION_H_
+
+#include <concord/discord_codecs.h>
+
+const struct discord_create_global_application_command CAPTION_PARAMS = {
+
+};
+
+#endif // DISCORD_CAPTION_H_
--- discord/main.c
+++ discord/main.c
@@ -1,6 +1,15 @@
 
 #include <concord/discord.h>
+#include <concord/discord_codecs.h>
 #include <stdlib.h>
+
+void bot_on_ready(struct discord *client, const struct discord_ready *event) {
+    struct discord_create_global_application_command params = {
+        .name = "caption",
+        .description = "put text on an image, ifunny GIF caption/esmBot style",
+    };
+    discord_create_global_application_command(client, event->application->id, &params, NULL);
+}
 
 int main(void) {
 	const char *bot_token = getenv("DISCORD_BOT_TOKEN");
@@ -10,6 +19,8 @@ int main(void) {
 	}
 
 	struct discord *client = discord_init(bot_token);
+    
+    discord_set_on_ready(client, bot_on_ready);
 	discord_run(client);
 
 	return 0;
